72f31fffb984e1c20ec29620c26db907af4c141a
Cedric Baude <baude.cedric.perso@gmail.com>
2023-01-30 15:24:31 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>React Multi Carousel</h1>
+
+        </div>
+    );
+}
+
+export default App;
