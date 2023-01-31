67e8037dc364f0112fb41fa192483a89e8bc4e26
Cedric Baude <baude.cedric.perso@gmail.com>
2023-01-31 20:16:02 +0100

    add card

--- src/App.js
+++ src/App.js
@@ -3,10 +3,40 @@ import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 
 function App() {
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1
+        }
+    };
     return (
         <div className="App">
             <h1>React Multi Carousel</h1>
-
+            <Carousel responsive={responsive}>
+                <div className='card'>
+                    <img className='product--image' src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZHVjdHN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="product_image" />
+                    <h2>Sport sneaker</h2>
+                    <p className='price'>$20,99</p>
+                    <p>Some description about product ..</p>
+                    <button>Add to cart</button>
+                </div>
+                <div>Item 2</div>
+                <div>Item 3</div>
+                <div>Item 4</div>
+            </Carousel>
         </div>
     );
 }
